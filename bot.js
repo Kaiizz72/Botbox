8cc217f7ac93391e726c1bba2c635e1625c70f6d
cuongaz25 <huydo1388@gmail.com>
2025-10-17 14:43:38 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const { GoalNear } = goals
 const collectBlockPlugin = require('mineflayer-collectblock').plugin
 const pvpPlugin = require('mineflayer-pvp').plugin
 
-const SERVER_HOST = process.env.SERVER_HOST || 'darknight.asakahost.net'
+const SERVER_HOST = process.env.SERVER_HOST || 'echomc.asia'
 const SERVER_PORT = Number(process.env.SERVER_PORT || 25565)
 const AUTH_MODE   = process.env.AUTH_MODE || 'offline'
 const PASSWORD    = process.env.BOT_PASS || '11qqaa22wwss'
